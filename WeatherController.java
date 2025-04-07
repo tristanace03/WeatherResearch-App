ef4d30c74702a0c371a8e0ad385123f8eb9c615f
Tristan Cravens <126623655+tristanace03@users.noreply.github.com>
2025-04-07 17:08:30 -0500

    Create WeatherController.java

---
+++ WeatherController.java
@@ -0,0 +1,27 @@
+public class WeatherController{
+  private LogbookManager logbookManager;
+  private DatabaseManager databaseManager;
+  private UserManager userManager;
+
+  public addObservation{
+  }
+
+  public getObservation{
+  }
+
+  public saveObservation{
+  }
+
+  public queryObservations{
+  }
+
+  public saveUserPreferences{
+  }
+
+  public getUserPreferences{
+  }
+
+  public getWeatherData{
+  }
+
+}
