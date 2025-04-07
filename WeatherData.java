b213d6f26358195de0515cbcbde94795ecd9c90c
Evan Rogers <evanrgrs@gmail.com>
2025-04-07 12:52:59 -0500

    WeatherData Initial Code

--- WeatherData.java
+++ WeatherData.java
@@ -1,3 +1,18 @@
 public class WeatherData{
   //represents the real time weather data (temp, humidity, etc.)
+
+  private double temperature;
+  private double humidity;
+  private double windSpeed;
+  private String condition;
+
+  /**
+   * Method to Format and override the toString() Method
+   * 
+   * @return String
+   */
+  @Override
+  public String toString(){
+    return "Temperature: " + this.temperature + ", Humidity: " + this.humidity + ", Wind Speed: " + this.windSpeed + ", Condition: " + this.condition;
+  }
 }
