d0e2a277e3ddcc3ee47c731e3a54daf451833231
dea2414 <143860627+dea2414@users.noreply.github.com>
2025-04-07 19:39:24 -0500

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -3,4 +3,31 @@ import java.util.Map;
 
 public class UserManager{
   //handles login, preferenecs and favorite locations 
+    private Map<String, String> users;
+    private Map<String, String> preferences;
+
+    public UserManager() {
+        users = new HashMap<>();
+        preferences = new HashMap<>();
+    }
+
+    public void addUser(String username, String password) {
+        users.put(username, password);
+    }
+
+    public boolean login(String username, String password) {
+        return users.containsKey(username) && users.get(username).equals(password);
+    }
+
+    public void setPreference(String username, String preference) {
+        preferences.put(username, preference);
+    }
+
+    public String getPreference(String username) {
+        if (preferences.containsKey(username)) {
+            return preferences.get(username);
+        } else {
+            return "no preference set";
+        }
+    }
 }
