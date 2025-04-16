fe4143f5530f0bc306c27be51f6dc21e3449e68e
Evan Rogers <evanrgrs@gmail.com>
2025-04-16 13:03:51 -0500

    structure updates

--- WeatherResearchApp.java
+++ WeatherResearchApp.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class WeatherResearchApp{
   //main class for application
   public static void main(String[] args){
