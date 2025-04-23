121bdfed2faed5ccd1cd6a71f9a48830aa59e20c
Aaron Sihweil <ASihweil1219@gmail.com>
2025-04-23 16:18:23 -0500

    Adding export logs and WeatherDataFactory

--- WeatherResearchApp.java
+++ WeatherResearchApp.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Date;
 import java.util.Scanner;
 
 
 public class WeatherResearchApp{
   //main class for application
   public static void main(String[] args){
-
+    
   }
 
 }
